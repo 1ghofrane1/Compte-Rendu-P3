3d9934c22196814cc6a5b41bd690cc4fa6224bd9
1ghofrane1 <ghofranebouallegue5@gmail.com>
2025-02-26 20:56:24 +0100

    data

--- src/pages/api/authors.js
+++ src/pages/api/authors.js
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 
-const dataFilePath = path.join(process.cwd(),'', 'authors.json');
+const dataFilePath = path.join(process.cwd(),'data', 'authors.json');
 
 async function readAuthorsData() {
   const data = await fs.readFile(dataFilePath, 'utf8');
